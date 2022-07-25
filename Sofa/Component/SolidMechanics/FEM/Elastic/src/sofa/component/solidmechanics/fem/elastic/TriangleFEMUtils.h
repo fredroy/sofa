7d5fa3d11b9d62de4af404e7af12c1b85e2713bc
Frederick Roy <froy@lnrobo.com>
2022-07-25 12:02:44 +0900

    inline

--- Sofa/Component/SolidMechanics/FEM/Elastic/src/sofa/component/solidmechanics/fem/elastic/TriangleFEMUtils.h
+++ Sofa/Component/SolidMechanics/FEM/Elastic/src/sofa/component/solidmechanics/fem/elastic/TriangleFEMUtils.h
@@ -49,22 +49,155 @@ public:
     typedef sofa::core::topology::BaseMeshTopology::Index Index;
 
     ////////////// small displacements method
-    static void computeDisplacementSmall(Displacement& D, const type::fixed_array<Coord, 3>& rotatedInitCoord, const Coord& pAB, const Coord& pAC);
+    static void computeDisplacementSmall(Displacement& D, const type::fixed_array<Coord, 3>& rotatedInitCoord, const Coord& pAB, const Coord& pAC)
+    {
+        D[0] = 0;
+        D[1] = 0;
+        D[2] = rotatedInitCoord[1][0] - pAB[0];
+        D[3] = rotatedInitCoord[1][1] - pAB[1];
+        D[4] = rotatedInitCoord[2][0] - pAC[0];
+        D[5] = rotatedInitCoord[2][1] - pAC[1];
+    }
 
     ////////////// large displacements method
-    static void computeDisplacementLarge(Displacement& D, const Transformation& R_0_2, const type::fixed_array<Coord, 3>& rotatedInitCoord,const Coord& pA, const Coord& pB, const Coord& pC);
-    static void computeRotationLarge(Transformation& r, const Coord& pA, const Coord& pB, const Coord& pC);
-    static void computeForceLarge(Displacement& F, const StrainDisplacement& J, const type::Vec<3, Real>& stress);
+    static void computeDisplacementLarge(Displacement& D, const Transformation& R_0_2, const type::fixed_array<Coord, 3>& rotatedInitCoord,const Coord& pA, const Coord& pB, const Coord& pC)
+    {
+        // positions of the deformed and displaced triangle in its local frame
+        const Coord deforme_b = R_0_2 * (pB - pA);
+        const Coord deforme_c = R_0_2 * (pC - pA);
+
+        // displacements in the local frame
+        D[0] = 0;
+        D[1] = 0;
+        D[2] = rotatedInitCoord[1][0] - deforme_b[0];
+        D[3] = 0;
+        D[4] = rotatedInitCoord[2][0] - deforme_c[0];
+        D[5] = rotatedInitCoord[2][1] - deforme_c[1];
+    }
+    static void computeRotationLarge(Transformation& r, const Coord& pA, const Coord& pB, const Coord& pC)
+    {
+        const Coord edgex = (pB - pA).normalized();
+        Coord edgey = pC - pA;
+        const Coord edgez = cross(edgex, edgey).normalized();
+        edgey = cross(edgez, edgex); //edgey is unit vector because edgez and edgex are orthogonal unit vectors
+
+        r[0][0] = edgex[0];
+        r[0][1] = edgex[1];
+        r[0][2] = edgex[2];
+        r[1][0] = edgey[0];
+        r[1][1] = edgey[1];
+        r[1][2] = edgey[2];
+        r[2][0] = edgez[0];
+        r[2][1] = edgez[1];
+        r[2][2] = edgez[2];
+    }
+
+    static void computeForceLarge(Displacement& F, const StrainDisplacement& J, const type::Vec<3, Real>& stress)
+    {
+        F[0] = J[0][0] * stress[0] + J[0][2] * stress[2];
+        F[1] = J[1][1] * stress[1] + J[1][2] * stress[2];
+        F[2] = J[2][0] * stress[0] + J[2][2] * stress[2];
+        F[3] = J[3][1] * stress[1] + J[3][2] * stress[2];
+        F[4] = J[4][2] * stress[2];
+        F[5] = J[5][1] * stress[1];
+    }
     
     // in global coordinate
-    static void computeStrainDisplacementGlobal(StrainDisplacement& J, const Coord& pA, const Coord& pB, const Coord& pC);
+    static void computeStrainDisplacementGlobal(StrainDisplacement& J, const Coord& pA, const Coord& pB, const Coord& pC)
+    {
+        const Coord ab_cross_ac = cross(pB - pA, pC - pA);
+        const Real determinant = ab_cross_ac.norm();
+
+        if (fabs(determinant) < std::numeric_limits<Real>::epsilon())
+        {
+            msg_error("TriangleFEMUtils") << "Null determinant in computeStrainDisplacementGlobal: " << determinant;
+            throw std::logic_error("Division by zero exception in computeStrainDisplacementGlobal ");
+        }
+
+        const Real invDet = 1 / determinant;
+
+        const Real x13 = (pA[0] - pC[0]) * invDet;
+        const Real x21 = (pB[0] - pA[0]) * invDet;
+        const Real x32 = (pC[0] - pB[0]) * invDet;
+        const Real y12 = (pA[1] - pB[1]) * invDet;
+        const Real y23 = (pB[1] - pC[1]) * invDet;
+        const Real y31 = (pC[1] - pA[1]) * invDet;
+
+        J[0][0] = y23;
+        J[0][1] = 0;
+        J[0][2] = x32;
+
+        J[1][0] = 0;
+        J[1][1] = x32;
+        J[1][2] = y23;
+
+        J[2][0] = y31;
+        J[2][1] = 0;
+        J[2][2] = x13;
+
+        J[3][0] = 0;
+        J[3][1] = x13;
+        J[3][2] = y31;
+
+        J[4][0] = y12;
+        J[4][1] = 0;
+        J[4][2] = x21;
+
+        J[5][0] = 0;
+        J[5][1] = x21;
+        J[5][2] = y12;
+    }
     // in local coordinate, a = Coord (0, 0, 0)
-    static void computeStrainDisplacementLocal(StrainDisplacement& J, const Coord& pB, const Coord& pC);
+    static void computeStrainDisplacementLocal(StrainDisplacement& J, const Coord& pB, const Coord& pC)
+    {
+        // local computation taking into account that a = [0, 0, 0], b = [x, 0, 0], c = [y, y, 0]
+        const Real determinant = pB[0] * pC[1];
+
+        if (fabs(determinant) < std::numeric_limits<Real>::epsilon())
+        {
+            msg_error("TriangleFEMUtils") << "Null determinant in computeStrainDisplacementGlobal: " << determinant;
+            throw std::logic_error("Division by zero exception in computeStrainDisplacementLocal");
+        }
+        const Real invDet = 1 / determinant;
+
+        J[0][0] = J[1][2] = -pC[1] * invDet;
+        J[0][2] = J[1][1] = (pC[0] - pB[0]) * invDet;
+        J[2][0] = J[3][2] = pC[1] * invDet;
+        J[2][2] = J[3][1] = -pC[0] * invDet;
+        J[4][0] = J[5][2] = 0;
+        J[4][2] = J[5][1] = pB[0] * invDet;
+        J[1][0] = J[3][0] = J[5][0] = J[0][1] = J[2][1] = J[4][1] = 0;
+    }
 
     // Compute strain, if full is set to true, full matrix multiplication is performed not taking into account potential 0 values
-    static void computeStrain(type::Vec<3, Real>& strain, const StrainDisplacement& J, const Displacement& D, bool fullMethod = false);
+    static void computeStrain(type::Vec<3, Real>& strain, const StrainDisplacement& J, const Displacement& D, bool fullMethod = false)
+    {
+        if (fullMethod) // _anisotropicMaterial or SMALL case
+        {
+            strain = J.multTranspose(D);
+        }
+        else
+        {
+            // Use directly J to avoid computing Jt
+            strain[0] = J[0][0] * D[0] + J[2][0] * D[2];
+            strain[1] = J[1][1] * D[1] + J[3][1] * D[3] + J[5][1] * D[5];
+            strain[2] = J[0][2] * D[0] + J[1][2] * D[1] + J[2][2] * D[2] + J[3][2] * D[3] + J[4][2] * D[4];
+        }
+    }
     // Compute stress, if full is set to true, full matrix multiplication is performed not taking into account potential 0 values
-    static void computeStress(type::Vec<3, Real>& stress, const MaterialStiffness& K, const type::Vec<3, Real>& strain, bool fullMethod = false);
+    static void computeStress(type::Vec<3, Real>& stress, const MaterialStiffness& K, const type::Vec<3, Real>& strain, bool fullMethod = false)
+    {
+        if (fullMethod) // _anisotropicMaterial or SMALL case
+        {
+            stress = K * strain;
+        }
+        else
+        {
+            stress[0] = K[0][0] * strain[0] + K[0][1] * strain[1];
+            stress[1] = K[1][0] * strain[0] + K[1][1] * strain[1];
+            stress[2] = K[2][2] * strain[2];
+        }
+    }
 };
 
 #if !defined(SOFA_COMPONENT_FORCEFIELD_TRIANGLEFEMUTILS_CPP)
