82519b1b520d6bf5f26fc1be6b3eaae7c29e2b39
Frederick Roy <froy@lnrobo.com>
2024-02-27 15:47:02 +0900

    wip

--- Sofa/framework/Simulation/Core/src/sofa/simulation/DefaultTaskScheduler.h
+++ Sofa/framework/Simulation/Core/src/sofa/simulation/DefaultTaskScheduler.h
@@ -78,7 +78,7 @@ public:
             
     static DefaultTaskScheduler* create();
 
-    std::mutex s_addWorkerThread;
+    inline static std::mutex s_addWorkerThread {};
     WorkerThread* addWorkerThread(std::size_t index = 0, const std::string& id = "Main  ");
 private:
             
