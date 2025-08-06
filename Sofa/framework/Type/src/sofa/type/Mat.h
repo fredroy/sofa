053cd90474a77d6028967a62c29192b7f57e9b55
Frederick Roy <froy@lnrobo.com>
2025-08-07 08:34:34 +0900

    compile FEM.NonUni

--- Sofa/framework/Type/src/sofa/type/Mat.h
+++ Sofa/framework/Type/src/sofa/type/Mat.h
@@ -158,7 +158,7 @@ public:
     {
         for( Size i=0; i<L; i++ )
             for( Size j=0; j<C; j++ )
-                this->elems(i,j) = v;
+                (*this)(i,j) = v;
     }
 
     /// Constructor from another matrix
